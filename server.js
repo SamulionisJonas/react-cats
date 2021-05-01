800149bcf0e7e28592bbad36595d8dc70fcc5bb1
Jonas Samulionis <jonas.samulionis@outlook.com>
2021-05-01 10:40:35 +0300

    init server.js

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const cors = require('cors');
 
+// Init
+const app = express();
+
+// Cors
+app.use(cors());
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Test route
+app.get('/', (req, res) => res.send('Cats api running'));
+
+// Routes
+app.use('/api/cats', require('./routes/cats'));
+
+// Default PORT
+const PORT = process.env.PORT || 5000;
+
+// Start 
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
