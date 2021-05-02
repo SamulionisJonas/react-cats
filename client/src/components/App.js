debe142f89f3002d0a341e35f7363544c4c3e65e
Jonas Samulionis <jonas.samulionis@outlook.com>
2021-05-02 12:35:31 +0300

    create searchBar and filter structure

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+// Redux
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCatBreeds, search } from '../slices/catSlice';
+// Components
+import { SearchBar } from '../components/SearchBar/SearchBar';
 
 export const App = () => {
-    return (
-        <div className='wrapper'>
-            
-        </div>
-    )
-}
+	const dispatch = useDispatch();
+	const { loading, filtered } = useSelector((state) => state.catBreeds);
+
+	useEffect(() => {
+		dispatch(fetchCatBreeds());
+	}, [dispatch]);
+
+	return (
+		<div className='wrapper'>
+			<SearchBar onChange={search} />
+		</div>
+	);
+};
--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Filter } from "./Filter";
 
-export const SearchBar = () => {
-    return (
-        <div>
-            
+export const SearchBar = ({ onChange }) => {
+  // Input serchTerm
+  const [searchTerm, setSearchTerm] = React.useState("");
+  // Filter state
+  const [state, setState] = React.useState("name");
+
+  return (
+    <>
+      <div className="searchbar">
+        <div className="searchbar_input">
+          <input
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder={`Search by ${state}`}
+          />
         </div>
-    )
-}
+      </div>
+      <Filter value={state} onChange={setState} />
+    </>
+  );
+};
