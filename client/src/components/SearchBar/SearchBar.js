14d131b6dbbbfa2e9d303910825eb829d7187bc0
Jonas Samulionis <jonas.samulionis@outlook.com>
2021-05-02 14:35:06 +0300

    add searchBar actions

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,23 +1,37 @@
-import React, { useEffect } from "react";
-import { Filter } from "./Filter";
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useDebounce } from '../../utils/useDebounce';
+import { Filter } from './Filter';
 
 export const SearchBar = ({ onChange }) => {
-  // Input serchTerm
-  const [searchTerm, setSearchTerm] = React.useState("");
-  // Filter state
-  const [state, setState] = React.useState("name");
+	// Input serchTerm
+	const [searchTerm, setSearchTerm] = React.useState('');
+	// Filter state
+	const [state, setState] = React.useState('name');
+	const dispatch = useDispatch();
 
-  return (
-    <>
-      <div className="searchbar">
-        <div className="searchbar_input">
-          <input
-            onChange={(e) => setSearchTerm(e.target.value)}
-            placeholder={`Search by ${state}`}
-          />
-        </div>
-      </div>
-      <Filter value={state} onChange={setState} />
-    </>
-  );
+	const debouncedValue = useDebounce(searchTerm, 300);
+
+	useEffect(() => {
+		dispatch(
+			onChange({
+				value: searchTerm,
+				sortBy: state,
+			})
+		);
+	}, [debouncedValue]);
+
+	return (
+		<>
+			<div className='searchbar'>
+				<div className='searchbar_input'>
+					<input
+						onChange={(e) => setSearchTerm(e.target.value)}
+						placeholder={`Search by ${state}`}
+					/>
+				</div>
+			</div>
+			<Filter value={state} onChange={setState} />
+		</>
+	);
 };
